3efc37e43cfe9f41dbcfb8e0136b9579584a97ee
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-08-05 20:02:03 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -12,7 +12,7 @@ def nyc_pigeon_organizer(data)
           pigeon_names.each do |names|
             #binding.pry
               city_pigeons[names] = {} if city_pigeons[names].nil?
-              new_hash[pigeon][attribute_type] = [] if new_hash[pigeon][attribute_type].nil?
+              city_pigeons[names][pigeon_attribute] = [] if city_pigeons[names][pigeon_attribute].nil?
           end
       end
   end
