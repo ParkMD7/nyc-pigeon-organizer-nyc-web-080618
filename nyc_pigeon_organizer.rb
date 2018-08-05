617313a714dc291c595e794b4aacdde26d477188
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-08-05 20:07:19 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+require 'pry'
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+  city_pigeons = {}
+  
+  data.each do |pigeon_attribute, pigeon_colors|
+    #binding.pry
+      pigeon_colors.each do |attribute_detail, pigeon_names|
+        #binding.pry
+          pigeon_names.each do |names|
+            #binding.pry
+              city_pigeons[names] = {} if city_pigeons[names].nil?
+              city_pigeons[names][pigeon_attribute] = [] if city_pigeons[names][pigeon_attribute].nil?
+              city_pigeons[names][pigeon_attribute] << attribute_detail.to_s
+          end
+      end
+  end
+  city_pigeons
 end
